9a4ee4acd664eeec77399c3597e6126785dd07a9
vivek9985 <vivekanando363@gmail.com>
2025-06-13 23:10:23 +0600

    services page modified

--- src/Components/Card/ServiceCard.tsx
+++ src/Components/Card/ServiceCard.tsx
@@ -14,7 +14,9 @@ export default function ServiceCard({ icon, title, paragraph, className, underCl
     return (
         <div className={`bg-neutral-200 border-[1px] border-neutral-200 group ${className}`}>
             <div className={`bg-white group-hover:rounded-tl-2xl group-hover:rounded-tr-2xl group-hover:bg-success-500 py-12 px-8 text-center duration-300 hover:-translate-y-2.5 hover:translate-x-2.5 ${underClassName}`}>
-                <Image className="mx-auto select-none" src={icon} alt="Icon" width={56} height={56} />
+                <div className="w-14 h-14 flex items-center justify-center rounded-full bg-success-500 group-hover:bg-white duration-300 mx-auto select-none">
+                    <Image className="w-6 h-6 object-contain group-hover:invert duration-300" src={icon} alt="Icon" width={24} height={24} />
+                </div>
                 <Heading variant="h4" className="mt-10 mb-2.5 text-primary-900 group-hover:text-white">{title}</Heading>
                 <Paragraph variant="para-7" className="text-neutral-600 group-hover:text-neutral-100">{paragraph}</Paragraph>
             </div>
--- src/Components/Section/Services/Hero.tsx
+++ src/Components/Section/Services/Hero.tsx
@@ -8,6 +8,11 @@ import GradientBg from "@/assets/images/page-header-bg.svg";
 import HalfCircle from "@/assets/images/half-circle.png";
 import Spring from "@/assets/images/spring.svg";
 import IconOne from "@/assets/images/icons/service-icon-1.svg";
+import IconTwo from "@/assets/images/icons/service-icon-2.svg";
+import IconThree from "@/assets/images/icons/service-icon-3.svg";
+import IconFour from "@/assets/images/icons/service-icon-4.svg";
+import IconFive from "@/assets/images/icons/service-icon-5.svg";
+import IconSix from "@/assets/images/icons/service-icon-6.svg";
 import ServiceCard from "@/Components/Card/ServiceCard";
 import ShinyText from "@/animation/ShinyText";
 import { motion } from 'framer-motion';
@@ -54,11 +59,11 @@ export default function Hero() {
                 transition={{delay: 1.2, duration: 0.5 }} className="mb-20 relative">
                 <Container className="grid sm:grid-cols-2 lg:grid-cols-3 mt-20 relative z-10 shadow-[8px_12px_68px_rgba(0,0,0,0.05)]">
                     <ServiceCard className="lg:rounded-tl-2xl" underClassName="lg:rounded-tl-2xl" icon={IconOne} title="Development" paragraph="Thousands of Developer" />
-                    <ServiceCard icon={IconOne} title="Design" paragraph="Thousands of Developer" />
-                    <ServiceCard className="lg:rounded-tr-2xl" underClassName="lg:rounded-tr-2xl" icon={IconOne} title="QA/Testing" paragraph="Thousands of Developer" />
-                    <ServiceCard className="lg:rounded-bl-2xl" underClassName="lg:rounded-bl-2xl group-hover:rounded-bl-[0px]" icon={IconOne} title="Creatives" paragraph="Thousands of Developer" />
-                    <ServiceCard icon={IconOne} title="Marketing" paragraph="Thousands of Developer" />
-                    <ServiceCard className="lg:rounded-br-2xl" underClassName="lg:rounded-br-2xl group-hover:rounded-br-[0px]" icon={IconOne} title="Research" paragraph="Thousands of Developer" />
+                    <ServiceCard icon={IconTwo} title="Design" paragraph="Thousands of Developer" />
+                    <ServiceCard className="lg:rounded-tr-2xl" underClassName="lg:rounded-tr-2xl" icon={IconThree} title="QA/Testing" paragraph="Thousands of Developer" />
+                    <ServiceCard className="lg:rounded-bl-2xl" underClassName="lg:rounded-bl-2xl group-hover:rounded-bl-[0px]" icon={IconFour} title="Creatives" paragraph="Thousands of Developer" />
+                    <ServiceCard icon={IconFive} title="Marketing" paragraph="Thousands of Developer" />
+                    <ServiceCard className="lg:rounded-br-2xl" underClassName="lg:rounded-br-2xl group-hover:rounded-br-[0px]" icon={IconSix} title="Research" paragraph="Thousands of Developer" />
                 </Container>
                 <div className="w-full h-[50%] bg-white absolute left-0 bottom-0 z-0"></div>
             </motion.div>
