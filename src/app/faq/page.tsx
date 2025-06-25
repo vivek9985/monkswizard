1b01ace319e6d2b49d4009c62d35a47be6e67398
vivek9985 <vivekanando363@gmail.com>
2025-06-25 22:35:25 +0600

    modified faq page

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -9,9 +9,9 @@ export const metadata: Metadata = {
 
 export default function FaqPage() {
   return (
-    <main>
+    <div>
       <PageHeader subtitle="FAQ" title="Frequently Asked Questions" paragraph="Our Frequently Asked Questions (FAQ) section is designed to provide quick and helpful answers to common inquiries about our services, processes, and policies. Whether you're curious about our pricing, turnaround times." />
       <Faq />
-    </main>
+    </div>
   );
 }
