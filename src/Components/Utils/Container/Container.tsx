6278d0ced93edbf559c8680d944aaf737c749261
vivek9985 <vivekanando363@gmail.com>
2025-06-28 23:24:04 +0600

    styles in container

--- src/Components/Utils/Container/Container.tsx
+++ src/Components/Utils/Container/Container.tsx
@@ -7,7 +7,7 @@ interface ContainerProps {
 
 export default function Container({ children, className }: ContainerProps) {
     return (
-        <div className={`max-w-[1140px] w-full mx-auto px-4 md:px-7 lg:px-5 xl:px-0 ${className}`}>
+        <div className={`max-w-[1140px] w-full mx-auto px-4 md:px-7 lg:px-6 xl:px-0 ${className}`}>
             {children}
         </div>
     );
